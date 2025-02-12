2b169876d745030de2c27e25400300c1ffcb2e9a
unknown <TRG@DI2INPUN5261WPC.ad001.siemens.net>
2025-02-12 17:09:53 +0530

    Your commit message here

--- devopsexam2025/main.tf
+++ devopsexam2025/main.tf
@@ -20,7 +20,7 @@ variable "AWS_REGION" {
 # Create a private subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id            = data.aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24" # Hardcoded CIDR block
+  cidr_block        = "10.0.3.0/24" # Hardcoded CIDR block
   availability_zone = "ap-south-1a" # Change this if needed
 
   tags = {
